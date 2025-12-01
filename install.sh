82ce7c2c7f26b2ebb0036e53c5fcf741977ac523
somostro <unknown001.js@gmail.com>
2025-12-01 19:50:29 +0530

    Bug fixes: Bash source file execution issues. Changes: install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ echo "==============================="
 echo -e "${CLR_RESET}"
 
 # Resolve project root
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$SCRIPT_DIR"
 
 # Detect Termux
